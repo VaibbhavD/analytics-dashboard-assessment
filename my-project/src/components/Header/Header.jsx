36878b21759bd08fd85fb6d113a7d75d1764020a
Vaibhav D <vaibhavdhamanage12@gmail.com>
2025-01-15 15:15:21 +0530

    Add Logo

--- my-project/src/components/Header/Header.jsx
+++ my-project/src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Logo from "../../assets/logo.png";
 
 const Header = ({ onMenuClick }) => {
   return (
-    <header className="bg-blue-600 text-white px-4 py-3 shadow-lg flex justify-between items-center fixed top-0 left-0 w-full z-50">
-      <h1 className="text-3xl font-bold">EV Dashboard</h1>
+    <header className="bg-blue-600 text-white px-4 py-2 shadow-lg flex items-center justify-between fixed top-0 gap-2 left-0 w-full z-50">
+      <div className="flex gap-2">
+        {" "}
+        <img src={Logo} width="70px" height="fit" loading="lazy" />
+        <h1 className="text-3xl font-bold pt-4">Dashboard</h1>
+      </div>
       <button onClick={onMenuClick} className="text-white text-3xl md:hidden">
         &#9776;
       </button>
